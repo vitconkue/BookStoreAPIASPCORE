53296d772781b71776dc1d2d58ea718198e6c0c5
vitconkue <voan0935995700@gmail.com>
2021-05-25 15:14:56 +0700

    add dbc context class

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models; 
+
+namespace BookStore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Book> Books {get;set;}
+    }
+}
