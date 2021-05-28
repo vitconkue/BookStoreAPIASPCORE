86cec16d6b0c8d4b953a8d260d3d3e10d086ffe1
vitconkue <voan0935995700@gmail.com>
2021-05-28 13:01:02 +0700

    Add Configuration Model

---
+++ Models/Configurations.cs
@@ -0,0 +1,11 @@
+namespace BookStore.Models
+{
+    public class Configuration
+    {
+        string Name {get;set;}
+        int Value {get;set;}
+        bool Status {get;set; }  = true; 
+    }
+
+  
+}
