cee6b09ef579907059c5d90f908fefff3101e070
vitconkue <voan0935995700@gmail.com>
2021-06-04 10:37:54 +0700

    add datetime in bookchanging record

--- Models/BookAmountChangingRecord.cs
+++ Models/BookAmountChangingRecord.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System;
 namespace BookStore.Models
 {
     public class BookAmountChangingRecord
@@ -16,6 +17,8 @@ namespace BookStore.Models
 
         [Required]
         public int AmountChanged {get;set;}
+
+        public DateTime DateChanged{get;set;} = DateTime.Now;
         
     }
 }
